355e93cc7e50917c313859b2518804fb066f3d62
alionachernenko <errorroror75@gmail.com>
2024-05-24 15:43:34 +0300

    add fixes

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ type Zoo struct {
 	Cages   []*Cage
 }
 
+func (zoo *Zoo) greetVisitors() {
+	fmt.Println("Hello! Welcome to the zoo. Unfortunately, today our animals decided to run away. Our zookeeper is trying to calm the situation down.\nRules: choose 1 or 0 to catch an animal")
+}
+
 type Animal struct {
 	Icon         rune
 	Species      string
@@ -27,104 +31,15 @@ type Animal struct {
 	DamagePoints int
 }
 
+func (animal *Animal) biteZookeperAndScreamOutLoud(zookeper *Zookeper) {
+	fmt.Printf("%c %s bites %s and screams out loud\n", animal.Icon, animal.Name, zookeper.Name)
+
+	zookeper.receiveDamageFromAnimal(animal)
+}
+
 type Zookeper struct {
 	Person
 	XP int
-}
-
-type Cage struct {
-	Size        string
-	Species     string
-	AnimalCount int
-}
-
-var animals = []*Animal{
-	{Species: "lion", Name: "Alex", Age: 10, Size: "Big", Caged: false, DamagePoints: 30, Icon: '\U0001f981'},
-	{Species: "zebra", Name: "Marty", Age: 10, Size: "Medium", Caged: false, DamagePoints: 20, Icon: '\U0001f993'},
-	{Species: "giraffe", Name: "Melman", Age: 11, Size: "Big", Caged: false, DamagePoints: 10, Icon: '\U0001f992'},
-	{Species: "hippopotamus", Name: "Gloria", Age: 10, Size: "Big", Caged: false, DamagePoints: 30, Icon: '\U0001f99b'},
-	{Species: "chimpanzee", Name: "Mason", Age: 5, Size: "Small", Caged: false, DamagePoints: 15, Icon: '\U0001f412'},
-	{Species: "chimpanzee", Name: "Phil", Age: 4, Size: "Small", Caged: false, DamagePoints: 15, Icon: '\U0001f412'},
-}
-
-var cages = []*Cage{
-	{
-		Size:        "big",
-		Species:     "lion",
-		AnimalCount: 0,
-	},
-	{
-		Size:        "big",
-		Species:     "zebra",
-		AnimalCount: 0,
-	},
-	{
-		Size:        "big",
-		Species:     "giraffe",
-		AnimalCount: 0,
-	},
-	{
-		Size:        "big",
-		Species:     "hippopotamus",
-		AnimalCount: 0,
-	},
-	{
-		Size:        "medium",
-		Species:     "chimpanzee",
-		AnimalCount: 0,
-	},
-}
-
-func main() {
-	zoo := Zoo{
-		Name:    "Central Park Zoo",
-		Cages:   cages,
-		Animals: animals,
-	}
-
-	zookeeperDavid := Zookeper{
-		Person: Person{
-			Name:    "David",
-			Surname: "Zoolander",
-			Age:     25,
-		},
-		XP: 100,
-	}
-
-	zoo.greetVisitors()
-
-	for {
-		if zookeeperDavid.XP <= 0 {
-			fmt.Printf("Game over!")
-			break
-		}
-
-		var escapedAnimals []*Animal
-
-		for _, animal := range zoo.Animals {
-			if !animal.Caged {
-				escapedAnimals = append(escapedAnimals, animal)
-			}
-		}
-
-		if len(escapedAnimals) == 0 {
-			fmt.Printf("All animals are now in cages! Good job, %s\n", zookeeperDavid.Name)
-			break
-		}
-
-		randomAnimal := escapedAnimals[rand.Intn(len(escapedAnimals))]
-
-		var currentCage *Cage
-
-		for _, cage := range zoo.Cages {
-			if cage.Species == randomAnimal.Species {
-				currentCage = cage
-				break
-			}
-		}
-
-		zookeeperDavid.catchAnimal(randomAnimal, currentCage)
-	}
 }
 
 func (zookeper *Zookeper) catchAnimal(animal *Animal, cage *Cage) {
@@ -133,7 +48,7 @@ func (zookeper *Zookeper) catchAnimal(animal *Animal, cage *Cage) {
 	randomInt := rand.Intn(2)
 
 	var userInput int
-	
+
 	fmt.Scan(&userInput)
 
 	if randomInt == userInput {
@@ -157,12 +72,110 @@ func (zookeper *Zookeper) receiveDamageFromAnimal(animal *Animal) {
 	}
 }
 
-func (animal *Animal) biteZookeperAndScreamOutLoud(zookeper *Zookeper) {
-	fmt.Printf("%c %s bites %s and screams out loud\n", animal.Icon, animal.Name, zookeper.Name)
-
-	zookeper.receiveDamageFromAnimal(animal)
+type Cage struct {
+	Size        string
+	Species     string
+	AnimalCount int
 }
 
-func (zoo *Zoo) greetVisitors() {
-	fmt.Println("Hello! Welcome to the zoo. Unfortunately, today our animals decided to run away. Our zookeeper is trying to calm the situation down.\nRules: choose 1 or 0 to catch an animal")
+func main() {
+	animals := []*Animal{
+		{Species: "lion", Name: "Alex", Age: 10, Size: "Big", Caged: false, DamagePoints: 30, Icon: '\U0001f981'},
+		{Species: "zebra", Name: "Marty", Age: 10, Size: "Medium", Caged: false, DamagePoints: 20, Icon: '\U0001f993'},
+		{Species: "giraffe", Name: "Melman", Age: 11, Size: "Big", Caged: false, DamagePoints: 10, Icon: '\U0001f992'},
+		{Species: "hippopotamus", Name: "Gloria", Age: 10, Size: "Big", Caged: false, DamagePoints: 30, Icon: '\U0001f99b'},
+		{Species: "chimpanzee", Name: "Mason", Age: 5, Size: "Small", Caged: false, DamagePoints: 15, Icon: '\U0001f412'},
+		{Species: "chimanzee", Name: "Phil", Age: 4, Size: "Small", Caged: false, DamagePoints: 15, Icon: '\U0001f412'},
+	}
+
+	cages := []*Cage{
+		{
+			Size:        "big",
+			Species:     "lion",
+			AnimalCount: 0,
+		},
+		{
+			Size:        "big",
+			Species:     "zebra",
+			AnimalCount: 0,
+		},
+		{
+			Size:        "big",
+			Species:     "giraffe",
+			AnimalCount: 0,
+		},
+		{
+			Size:        "big",
+			Species:     "hippopotamus",
+			AnimalCount: 0,
+		},
+		{
+			Size:        "medium",
+			Species:     "chimpanzee",
+			AnimalCount: 0,
+		},
+	}
+
+	zoo := Zoo{
+		Name:    "Central Park Zoo",
+		Cages:   cages,
+		Animals: animals,
+	}
+
+	zookeeperDavid := Zookeper{
+		Person: Person{
+			Name:    "David",
+			Surname: "Zoolander",
+			Age:     25,
+		},
+		XP: 100,
+	}
+
+	zoo.greetVisitors()
+
+	var escapedAnimals = zoo.Animals
+
+	for {
+		if zookeeperDavid.XP <= 0 {
+			fmt.Printf("Game over!")
+			break
+		}
+
+		for i, animal := range escapedAnimals {
+			if animal.Caged {
+				escapedAnimals = append(escapedAnimals[:i], escapedAnimals[i+1:]...)
+			}
+		}
+
+		if len(escapedAnimals) == 0 {
+			fmt.Printf("All animals are now in cages! Good job, %s\n", zookeeperDavid.Name)
+			break
+		}
+
+		randomAnimal := escapedAnimals[rand.Intn(len(escapedAnimals))]
+
+		var currentCage *Cage
+
+		for _, cage := range zoo.Cages {
+			if cage.Species == randomAnimal.Species {
+				currentCage = cage
+				break
+			}
+		}
+
+		if currentCage == nil {
+			fmt.Printf("Oops, there is no cage for %v", randomAnimal.Name)
+			randomAnimal.biteZookeperAndScreamOutLoud(&zookeeperDavid)
+
+			for i, a := range escapedAnimals {
+				if a.Name == randomAnimal.Name {
+					escapedAnimals = append(escapedAnimals[:i], escapedAnimals[i+1:]...)
+				}
+			}
+
+			continue
+		}
+
+		zookeeperDavid.catchAnimal(randomAnimal, currentCage)
+	}
 }
